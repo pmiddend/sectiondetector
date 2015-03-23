c73ccbff82eb9b0e9a70ffe66b114e490be81f57
Philipp Middendorf <pmidden@secure.mailbox.org>
2015-03-23 21:57:25 +0100

    Fix behavior

--- src/SectionDetector/DetectSections.hs
+++ src/SectionDetector/DetectSections.hs
@@ -53,10 +53,13 @@ playerCommand aspect start fp = ShellCommand $ playerCommandStr aspect start fp
 
 type TimeStamp = Double
 
-parseTimeStamp :: T.Text -> Maybe TimeStamp
+--parseTimeStamp :: T.Text -> Maybe TimeStamp
+parseTimeStamp = Just
+{-
 parseTimeStamp s = case double s of
   Left _ -> Nothing
   Right (x,_) -> Just x
+-}
 
 
 data Section = Section {
@@ -85,8 +88,8 @@ myProcess fp = CreateProcess {
 data MpvJson = MpvJson {
     mpvEventType :: T.Text
   , mpvEventName :: T.Text
-  , mpvEventData :: T.Text
-  }
+  , mpvEventData :: Double
+  } deriving(Show)
 
 instance FromJSON MpvJson where
   parseJSON (Object v) = MpvJson <$> (v .: "event") <*> (v .: "name") <*> (v .: "data")
@@ -109,12 +112,15 @@ mainLoop :: (MonadState PlayState m,MonadIO m) => Conduit TimeStamp m Section
 mainLoop = do
   timeStamp' <- await
   case timeStamp' of
-    Nothing -> return ()
+    Nothing -> do
+      ls <- use lastSeconds
+      csb <- use currentSectionBegin
+      yield (Section (floor csb) (floor ls))
+      return ()
     Just timeStamp -> do
-      liftIO $ TIO.putStrLn $ "Got " <> ( T.pack . show $ timeStamp )
       ls <- use lastSeconds
       when (timeStamp - ls > 1) $ do
-        liftIO $ TIO.putStrLn $ "Seeked!"
+        liftIO $ TIO.putStrLn "Seeked!"
         csb <- use currentSectionBegin
         yield (Section (floor csb) (floor ls))
         --sections <>= [Section csb ls]
@@ -130,80 +136,12 @@ toTimepos e =
 
 detectSections :: String -> Int -> FilePath -> IO [Section]
 detectSections aspect start filename = do
-  TIO.putStrLn $ T.pack $ "Running " <> (playerCommandStr aspect start filename)
-  output <- createProcessPretty (myProcess (playerCommand aspect start filename))
-  threadDelay ( 1000*1000 )
+  TIO.putStrLn $ T.pack $ "Running " <> playerCommandStr aspect start filename
+  _ <- createProcessPretty (myProcess (playerCommand aspect start filename))
+  threadDelay ( 500*1000 )
   runUnixClient (clientSettings sectionDetectorSocketFile) $ \app -> do
-    TIO.putStrLn "Yielding command"
     yield (encodeUtf8 "{ \"command\": [\"observe_property\", 1, \"time-pos\"]}\n") $$ appSink app
-    TIO.putStrLn "Done, reading"
     evalStateT ( appSource app $= CT.decode CT.utf8 =$= CT.lines =$= CL.map ( decode . BSL.fromStrict . encodeUtf8 ) =$= CL.catMaybes =$= CL.map toTimepos =$= CL.catMaybes =$= mainLoop $$ CL.consume ) initialPlayState
 
 filterSections :: Int -> [Section] -> [Section]
---filterSections ts = filter ((> ts) . (^. sectionDuration))
-filterSections ts = id
-
-{-
-
-type PlayStateMonad = StateT PlayState IO
-
-nextLine :: PlayStateMonad (Maybe Text)
-nextLine = do
-  h <- use streamHandle
---   Just <$> liftIO (hGetLine h)
-  tryAndDoNothing (hGetLine h)
-
-parseLine :: Text -> Maybe TimeStamp
-parseLine t = case decimal t of
-  Left _ -> Nothing
-  Right (a,_) -> Just a
-
-mainLoop :: PlayStateMonad ()
-mainLoop = do
-  line' <- nextLine
---   liftPutStrLn (pack (show line'))
-  case line' of
-    Nothing -> return ()
-    Just line -> case parseLine line of
-      Nothing -> liftPutStrLn ("unparsed: \"" <> line <> "\"") >> mainLoop
-      Just timeStamp -> do
-        ls <- use lastSeconds
-        when (timeStamp - ls > 1) $ do
-          liftPutStrLn "Seeked!"
-          csb <- use currentSectionBegin
-          sections <>= [Section csb ls]
-          currentSectionBegin .= timeStamp
-        lastSeconds .= timeStamp
-        mainLoop
-
-filterSections :: TimeStamp -> [Section] -> [Section]
 filterSections ts = filter ((> ts) . (^. sectionDuration))
-
-myProcess :: CmdSpec -> CreateProcess
-myProcess fp = CreateProcess {
-    cmdspec = fp
-  , cwd = Nothing
-  , env = Nothing
-  , std_in = Inherit
-  , std_out = Inherit
-  , std_err = Inherit
-  , close_fds = False
-  , create_group = False
-  , delegate_ctlc = False
-  }
-
-detectSections :: String -> Int -> FilePath -> IO [Section]
-detectSections aspect start filename = do
-  output <- createProcessPretty (myProcess (playerCommand aspect start filename))
-  --let ehandle = output ^?! (processErrHandle . _Just)
-  --hSetBuffering ehandle NoBuffering
-  let ps = PlayState {
-      _lastSeconds = 0
-    , _currentSectionBegin = 0
-    , _sections = []
-    , _streamHandle = ehandle
-    }
-  finalState <- execStateT mainLoop ps
-  return (finalState ^. sections)
-
--}
